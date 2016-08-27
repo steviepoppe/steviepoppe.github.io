7f58f19559df3c0dad066972390ca6e7ac055dde
steviepoppe <stevie_poppe@hotmail.com>
2016-08-27 16:10:49 +0100

    Update documentation

--- theme/js/footnotes.js
+++ theme/js/footnotes.js
@@ -7,7 +7,11 @@ var Footnotes = {
     footnotetimeout: false,
     setup: function() {
         var footnotelinks = $("a[rel='footnote']")
-        
+
+
+            $("a[rel='footnote']").on('click',Footnotes.footnotescroll);
+            $("a[rev='footnote']").on('click',Footnotes.footnotescroll);
+
         footnotelinks.unbind('mouseover',Footnotes.footnoteover);
         footnotelinks.unbind('mouseout',Footnotes.footnoteoout);
         
@@ -58,5 +62,53 @@ var Footnotes = {
         $('#footnotediv').css({
                 opacity: 0.9
         });
-    }
+    },
+
+
+    footnotescroll: function(e) {
+  var t, a, n, r, o, i;
+  if (location.pathname.replace(/^\//, "") === this.pathname.replace(/^\//, "") || location.hostname === this.hostname) {
+    if (e.preventDefault(), o = $("nav.navbar").height() + 32, $(this).hasClass("footnote-backref")) 
+        return i = "footnote-highlight", n = $(this), t = $('[id="' + n.attr("href").slice(1) + '"]'), 
+    a = t.closest("p, ul, ol"), a.addClass(i), a.one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend", 
+        function() {
+      return console.log("hello"), a.removeClass(i)
+    }), $("html,body").animate({
+      scrollTop: a.offset().top - o
+    }, 500), !1;
+    if ($(this).hasClass("footnote-ref")) {
+      if (r = this.hash.slice(1), n = $('[id="' + r + '"]'), n.length) return $("html,body").animate({
+        scrollTop: n.offset().top - o
+      }, 500), !1
+    } else if (n = $(this.hash), n = n.length ? n : $('[name="' + this.hash.slice(1)(NaN)), n.length) return $("html,body").animate({
+      scrollTop: n.offset().top - o
+    }, 500), !1
+  }
 }
+}
+
+$(document).ready(function(){
+
+
+    $('a[href^="#"]:not(a[rel="footnote"]):not(a[rev="footnote"]):not(a[id="backtotop"])').on('click',function (e) {
+        e.preventDefault();
+
+        var target = this.hash;
+        var $target = $(target);
+
+$('html, body').stop().animate({
+     'scrollTop': $target.offset().top
+}, 500, 'swing');
+    });
+
+        $('a[id="backtotop"]').on('click',function (e) {
+        e.preventDefault();
+
+        var target = this.hash;
+        var $target = $(target);
+
+$('html, body').stop().animate({ scrollTop: 0 }, 400, 'swing');
+    });
+});
+
+
